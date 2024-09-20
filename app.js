f446763b553ad0e0a5ab466562b88df7767234ec
mehukatti <janniinakarinsalo@gmail.com>
2024-09-20 21:55:39 +0300

    separate javascript from html

---
+++ app.js
@@ -0,0 +1,35 @@
+(function populateFromJson(){
+    const myList = document.querySelector("div");
+    var listPath = "data/git/git_not_in_your_mahcine.json"
+    fetch(listPath)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`Unable to fetch ${listPath}. Status = ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        for (const cheat of data.Cheats) {
+            const cheatBlockElement = document.createElement("div");
+            cheatBlockElement.className = "cheatBlock";
+
+            const explanationElement = document.createElement("p"); // Would like this to be a explanation element type
+            explanationElement.textContent = cheat.Explanation;
+
+            const cheatElement = document.createElement("div"); // Would like this to be a code block element type
+            cheatElement.className = "codeBlock";
+            cheatElement.textContent = cheat.Cheat;
+
+            cheatBlockElement.append(
+                explanationElement,
+                cheatElement
+        );
+        myList.appendChild(cheatBlockElement);
+        }
+    })
+    .catch((error) => {
+        const p = document.createElement("p");
+        p.appendChild(document.createTextNode(`Error: ${error.message}`));
+        document.body.insertBefore(p, myList);
+    });
+})
